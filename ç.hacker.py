cba693c5413340603403d980135b4211a9c660f7
çılgın hacker <98042176+termuxhackt@users.noreply.github.com>
2022-01-26 16:47:45 +0300

    hacker.py

---
+++ ç.hacker.py
@@ -0,0 +1,36 @@
+GNU nano 6.0                    ls.py                     Modified
+#!/user/bin/evn python
+
+import os
+
+os.system("pkg intall figlet")
+os.system("clear")
+os.system(" figlet  Hacker")
+os.system("clear")
+
+print(" hoşgeldin  ç.hacker ben hazırım!!")
+os.system(" figlet Hacker  ")
+print("""
+1)// TC SORGU
+2)// hakımızda                ###@@@@
+""")
+os.system(" figlet Hacker  ")
+giris=input("lütfen işlemno giriniz: ")
+if giris=="1":
+        os.system("clear")          //ç.hackere ôzel
+os.system("figlet hack TOOL ")
+os.system("clear")
+os.system("figlet HACK TOOL")
+print("""
+1) hakımızda
+2)TC sorgu devam et
+""")
+"/data/data/com.termux/files/home/ls.py"
+giris=input("lütfen işlemno giriniz: ")
+if giris=="2":
+        os.system("clear")
+print("işlem devam ediyor lûtfen bekleyiniz")
+os.system("figlet start")
+os.system("pkg install Hollywood")
+
+start/clear/sh/python/ç.hacker.py/////
